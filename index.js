0025551203c08db3ff38f9b415ecba56bf714c40
vincent <muehler.v@gmail.com>
2018-01-31 11:02:40 +0100

    ci fixes + export libs and include dir properly

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const getLibs = require('./libs')
 const {
   opencvInclude,
+  opencvLibDir,
   opencvBinDir,
   opencvModules
 } = require('./constants')
@@ -10,12 +12,19 @@ const {
   hasSelfBuild
 } = require('./install/utils')
 
+const resolvePath = (filePath, file) => {
+  if (!filePath) {
+    return undefined
+  }
+  return (file ? path.resolve(filePath, file) : path.resolve(filePath)).replace(/\\/g, '/')
+}
+
 function includes() {
   if (hasSelfBuild()) {
     return
   }
 
-  console.log(opencvInclude)
+  console.log(resolvePath(opencvInclude))
 }
 
 function libraries() {
@@ -23,7 +32,15 @@ function libraries() {
     return
   }
 
-  getLibs().map(lib => lib.libPath).forEach(lib => console.log(lib))
+  if (isWin()) {
+    getLibs().map(lib => lib.libPath).forEach(lib => console.log(resolvePath(lib)))
+    return
+  }
+
+  // if not windows, link libs dynamically
+  console.log('-L' + opencvLibDir)
+  console.log('-Wl,-rpath,' + opencvLibDir)
+  getLibs().map(lib => lib.libPath).forEach(lib => console.log('-lopencv_' + lib))
 }
 
 function ensureBinaries() {
