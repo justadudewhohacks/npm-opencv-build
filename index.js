66db10eabe8c352792a7b2a044a92530eb6fcf1c
vincent <muehler.v@gmail.com>
2018-03-27 12:02:58 +0200

    fix redefinition of opencvModules

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const fs = require('fs')
 const { isWin, isOSX } = require('./install/utils')
-const { opencvModules } = require('./constants')
-const getLibs = require('./libs')({ isWin, isOSX, opencvModules, path, fs })
 const {
   opencvInclude,
   opencvLibDir,
   opencvBinDir,
   opencvModules
 } = require('./constants')
+const getLibs = require('./libs')({ isWin, isOSX, opencvModules, path, fs })
+
 const {
   isAutoBuildDisabled
 } = require('./install/utils')
