17b43941898fa96b1a8293af7825034d0b44cacf
wouldgo <wouldgo84@gmail.com>
2019-08-05 23:44:21 +0200

    autobuild flags in package.json

--- install.js
+++ install.js
@@ -3,5 +3,22 @@ if (process.env.npm_config_loglevel === 'silly') {
 }
 
 const { install } = require('./build/install')
+  , {resolve} = require('path')
+  , rootPackageJSON = require(resolve(process.env.INIT_CWD, 'package.json'))
+
+if (rootPackageJSON.opencv4nodejs &&
+  rootPackageJSON.opencv4nodejs.flags) {
+
+  if (process.env.OPENCV4NODEJS_AUTOBUILD_FLAGS) {
+
+    process.env.OPENCV4NODEJS_AUTOBUILD_FLAGS = [
+      process.env.OPENCV4NODEJS_AUTOBUILD_FLAGS,
+      rootPackageJSON.opencv4nodejs.flags
+    ].join(' ')
+  } else {
+
+    process.env.OPENCV4NODEJS_AUTOBUILD_FLAGS = rootPackageJSON.opencv4nodejs.flags
+  }
+}
 
 install()
