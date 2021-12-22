84201c34e824f3188502d9b2aadb2aab2c8ce020
UrielCh <uriel.chemouni@gmail.com>
2021-12-22 22:24:40 +0200

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import { opencvModules } from './constants.js';
-import BuildContext from './BuildContext';
+import BuildContext from './BuildContext.js';
 import { getLibsFactory } from './getLibsFactory.js';
 import { isOSX, isWin, isUnix } from './utils.js';
 import { getLibs } from './getLibs.js';
@@ -11,6 +11,11 @@ import { default as env } from './env.js';
  */
 export type { OpencvModule, AutoBuildFile } from './types.js'
 
+export { opencvModules } from './constants.js';
+export { default as BuildContext } from './BuildContext.js';
+export { getLibsFactory } from './getLibsFactory.js';
+export { isOSX, isWin, isUnix } from './utils.js';
+export { getLibs } from './getLibs.js';
 export { default as env } from './env.js';
 
 export default  {
